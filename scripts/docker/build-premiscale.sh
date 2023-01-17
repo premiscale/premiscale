53ab985eceff7493ff1a727803c76099137797c8
Emma Doyle <brandon@sbevision.com>
2023-01-16 19:06:31 -0500

    Bump version of general-orb with docker build fix

--- scripts/docker/build-premiscale.sh
+++ scripts/docker/build-premiscale.sh
@@ -2,9 +2,9 @@
 # Docker build of the docker/autoscaler-directory.
 
 PYTHON_PACKAGE_VERSION="${1:-0.0.1a287}"
-PYTHON_USERNAME="$(pass show premiscale/nexus/username)"
-PYTHON_PASSWORD="$(pass show premiscale/nexus/password)"
-PYTHON_REPOSITORY="${2:-python-develop}"
+PYTHON_USERNAME="${2:-$(pass show premiscale/nexus/username)}"
+PYTHON_PASSWORD="${3:-$(pass show premiscale/nexus/password)}"
+PYTHON_REPOSITORY="${4:-python-develop}"
 
 
 docker build . -t docker.ops.premiscale.com/premiscale:"$PYTHON_PACKAGE_VERSION" --build-arg=PYTHON_PACKAGE_VERSION="$PYTHON_PACKAGE_VERSION" --build-arg=PYTHON_USERNAME="$PYTHON_USERNAME" --build-arg=PYTHON_PASSWORD="$PYTHON_PASSWORD" --build-arg=PYTHON_REPOSITORY="$PYTHON_REPOSITORY" -f docker/autoscaler/Dockerfile
