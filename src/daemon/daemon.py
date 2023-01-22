8a0e1b83acfb3a6f12d9088eeeaa7b8f2b663d07
Emma Doyle <brandon@sbevision.com>
2023-01-22 17:32:01 -0500

    Separate out daemons

--- src/daemon/daemon.py
+++ src/daemon/daemon.py
@@ -25,21 +25,32 @@ class PremiScaleDaemon:
         interval_evaluation (int > 0): interval that metrics from hosts should be evaluated, logic behind autoscaling decisions.
         interval_metrics (int > 0): interval that the metrics collection daemon thread should wake up to spawn threads that acquire data from the list of hosts.
         queue_max_size (int > 0): maximum number of hosts to keep on the shared queue.
-        queue_timeout (int > 0): maximum amount of time host data should reside on the queue.
     """
-    def __init__(self, interval_publish: int = 10, interval_metrics: int = 10, queue_max_size: int = 10, queue_timeout: int = 3600) -> None:
+    def __init__(self,
+                mysql_username: str,
+                mysql_password: str,
+                mysql_connection_string: str,
+                influx_username: str,
+                influx_password: str,
+                influx_connection_string: str,
+                interval_publish: int = 10,
+                interval_metrics: int = 10,
+                queue_max_size: int = 10,
+                queue_timeout: int = 3600
+                ) -> None:
         self.interval_publish = interval_publish
         self.interval_metrics = interval_metrics
 
         self.queue_max_size = queue_max_size
         self.queue_timeout = queue_timeout
 
-        self.queue: Queue = Queue(maxsize=queue_max_size)
+        self.platform_queue: Queue = Queue(maxsize=queue_max_size)
+        self.metrics_queue: Queue = Queue(maxsize=queue_max_size)
         self._n_threads = 0
 
         # Start a non-blocking daemon thread that periodically reads from the queue.
-        self._publish_daemon = Thread(target=self._d_publish, daemon=True)
-        self._publish_daemon.start()
+        self._platform_daemon = Thread(target=self._d_platform, daemon=True)
+        self._platform_daemon.start()
 
         # Start a non-blocking daemon thread that periodically writes to the queue.
         self._metrics_daemon = Thread(target=self._d_metrics, daemon=True)
@@ -57,30 +68,9 @@ class PremiScaleDaemon:
         Returns:
             Nothing.
         """
-        self.queue.put(js, timeout=self.queue_timeout)
+        self.metrics_queue.put(js, timeout=self.queue_timeout)
 
-    def _d_publish(self) -> None:
-        """
-        'Daemonized' publisher that has the ability to spin up other threads.
-        Periodically wakes up and tries to empty its queue of measurements.
-        This method is not meant to be called directly.
-        Returns:
-            Nothing.
-        """
-        while True:
-            if not self.queue.empty():
-                # No reason to create more threads than necessary, here.
-                if self.queue_max_size > self.queue.qsize():
-                    self._n_threads = self.queue.qsize()
-                    division = 1
-                    remainder = 0
-                else:
-                    self._n_threads = self.queue_max_size
-                    division = self.queue.qsize() // self._n_threads
-                    remainder = self.queue.qsize() % self._n_threads
-
-                self._spawn_threads(division, remainder)
-            sleep(self.interval_publish)
+    # Local metrics
 
     def _d_metrics(self) -> None:
         """
@@ -91,21 +81,21 @@ class PremiScaleDaemon:
             Nothing.
         """
         while True:
-            if not self.queue.empty():
+            if not self.metrics_queue.empty():
                 # No reason to create more threads than necessary, here.
-                if self.queue_max_size > self.queue.qsize():
-                    self._n_threads = self.queue.qsize()
+                if self.queue_max_size > self.metrics_queue.qsize():
+                    self._n_threads = self.metrics_queue.qsize()
                     division = 1
                     remainder = 0
                 else:
                     self._n_threads = self.queue_max_size
-                    division = self.queue.qsize() // self._n_threads
-                    remainder = self.queue.qsize() % self._n_threads
+                    division = self.metrics_queue.qsize() // self._n_threads
+                    remainder = self.metrics_queue.qsize() % self._n_threads
 
-                self._spawn_threads(division, remainder)
-            sleep(self.interval_metrics)
+                self._spawn_threads_metrics(division, remainder)
+            sleep(self.interval_publish)
 
-    def _spawn_threads(self, division: int, remainder: int =0) -> None:
+    def _spawn_threads_metrics(self, division: int, remainder: int =0) -> None:
         """
         Make publication threads, and return if there's an error during the request.
         Args:
@@ -116,22 +106,71 @@ class PremiScaleDaemon:
         """
         for _i in [division, remainder]:
             for _ in range(_i):
-                q_size_start = self.queue.qsize()
+                q_size_start = self.metrics_queue.qsize()
                 threads = []
                 for _ in range(self._n_threads):
-                    new_thread = Thread(target=self.t_publish, args=())
+                    new_thread = Thread(target=self._t_publish, args=())
                     new_thread.start()
                     threads.append(new_thread)
                 for thread in threads:
                     thread.join()
                 # If the ending queue size is the same as we started, just wait until
                 # the next publish cycle to try again, they're obviously failing.
-                if self.queue.qsize() == q_size_start:
+                if self.metrics_queue.qsize() == q_size_start:
                     return None
 
-    def t_publish(self) -> None:
+    # Platform metrics.
+
+    def _d_platform(self) -> None:
         """
-        Publish a datum point to the client.
+        'Daemonized' publisher that has the ability to spin up other threads.
+        Periodically wakes up and tries to empty its queue of measurements.
+        This method is not meant to be called directly.
+        Returns:
+            Nothing.
+        """
+        while True:
+            if not self.platform_queue.empty():
+                # No reason to create more threads than necessary, here.
+                if self.queue_max_size > self.platform_queue.qsize():
+                    self._n_threads = self.platform_queue.qsize()
+                    division = 1
+                    remainder = 0
+                else:
+                    self._n_threads = self.queue_max_size
+                    division = self.platform_queue.qsize() // self._n_threads
+                    remainder = self.platform_queue.qsize() % self._n_threads
+
+                self._spawn_threads_platform(division, remainder)
+            sleep(self.interval_metrics)
+
+    def _spawn_threads_platform(self, division: int, remainder: int =0) -> None:
+        """
+        Make publication threads, and return if there's an error during the request.
+        Args:
+            division: number of thread creation/join cycles to make during drain.
+            remainder: number of threads to create in the very last cycle.
+        Returns:
+            Nothing.
+        """
+        for _i in [division, remainder]:
+            for _ in range(_i):
+                q_size_start = self.platform_queue.qsize()
+                threads = []
+                for _ in range(self._n_threads):
+                    new_thread = Thread(target=self._t_publish, args=())
+                    new_thread.start()
+                    threads.append(new_thread)
+                for thread in threads:
+                    thread.join()
+                # If the ending queue size is the same as we started, just wait until
+                # the next publish cycle to try again, they're obviously failing.
+                if self.platform_queue.qsize() == q_size_start:
+                    return None
+
+    def _t_publish(self) -> None:
+        """
+        Publish a datum point to the platform.
         """
 
     def __enter__(self) -> 'PremiScaleDaemon':
