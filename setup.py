b98781f115ce04824f8f8c4a71045fedb19376fe
Emma Doyle <brandon@sbevision.com>
2023-01-26 18:47:04 -0500

    Wow

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'premiscale = premiscale.premiscale:cli',
+            'premiscale = premiscale:cli',
         ]
     },
     project_urls={  # Optional
