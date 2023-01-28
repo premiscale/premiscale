5630fb948c42b94db6bb4599477a9e8d5651d245
Emma Doyle <brandon@sbevision.com>
2023-01-27 23:34:54 -0500

    Add debug mode to docker container

--- docker/autoscaler/scripts/cmd.sh
+++ docker/autoscaler/scripts/cmd.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 # Start the backend api.
 
+PREMISCALE_DEBUG="${1:-false}"
+
 # shellcheck disable=SC1091
 source bin/activate
 
-premiscale --log-stdout --version
-premiscale --log-stdout --daemon
+if "$PREMISCALE_DEBUG"; then
+    premiscale --log-stdout --version --debug
+    premiscale --log-stdout --daemon --debug
+else
+    premiscale --log-stdout --version
+    premiscale --log-stdout --daemon
+fi
