70b07ea7b090793fdba96b542d8d50eb2cd9e4ae
Emma Doyle <brandon@sbevision.com>
2023-02-19 19:56:58 -0500

    ENG-132: Start work on sql connections

--- premiscale/premiscale/mysql.py
+++ premiscale/premiscale/mysql.py
@@ -2,6 +2,9 @@
 Methods for interacting with the MySQL database.
 """
 
-# from sqlalchemy.dialects.mysql import (
-#     insert
-# )
+from sqlalchemy.dialects.mysql import (
+    insert,
+)
+
+
+url = 'mysql+mysqldb://<user>:<password>@<host>[:<port>]/<dbname>'
