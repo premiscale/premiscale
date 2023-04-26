ac12da0195dd70eea8b96b9c6af4069982962c72
Emma Doyle <emma@sbevision.com>
2023-04-26 16:41:36 -0400

    ENG-172: Update general-orb, add development Dockerfiles

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,17 +1,23 @@
 #! /usr/bin/env bash
 # Docker run the docker/PROJECT-directory.
 
-PROJECT="${1:-premiscale}"
-VERSION="${2:-0.1.0}"
-shift && shift
+
+VERSION="${1:-0.1.0}"
+COUNT="${2:-1}"
+PROJECT="${3:-premiscale}"
+shift && shift && shift
 
 
-# docker run -itd --name "$PROJECT" docker.ops.premiscale.com/"$PROJECT":"$VERSION" "$@"
+mapfile -t containers < <(docker ps --format '{{.Names}}' | grep "$PROJECT")
+for container in "${containers[@]}"; do
+    docker stop "$container" >/dev/null && docker rm "$container" >/dev/null && printf "Removed running container \"%s\"\\n" "$container"
+done
 
-docker stop premiscale && docker rm premiscale
-docker run -itd --name "${PROJECT}" \
-    -e DOPPLER_TOKEN="$(pass show premiscale/doppler/development-service-token)" \
-    -e PREMISCALE_DEBUG=true \
-    -e PREMISCALE_TOKEN=123 \
-    docker-develop.ops.premiscale.com/"${PROJECT}":"${VERSION}" "$@"
-docker exec -it "${PROJECT}" /bin/bash
+
+for _ in $(seq 1 "$COUNT"); do
+    docker run -itd --name "${PROJECT}"-"$(uuid | sed "s@-@@g" | head -c 10)" \
+        -e DOPPLER_TOKEN="$(pass show premiscale/doppler/development-service-token)" \
+        -e PREMISCALE_DEBUG=true \
+        -e PREMISCALE_TOKEN=123 \
+        docker-develop.ops.premiscale.com/"${PROJECT}":"${VERSION}" "$@"
+done
