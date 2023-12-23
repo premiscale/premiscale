080b6f783399e74649cdfe4e7a1e8c371d0cbbe2
Emma Doyle <emma@sbevision.com>
2023-12-23 12:01:57 -0700

    working: fix importlib import paths

--- src/premiscale/config/parse.py
+++ src/premiscale/config/parse.py
@@ -82,7 +82,7 @@ def validate(config: Union[Path, str], schema: str = 'schema.yaml', strict: bool
     Returns:
         bool: Whether or not the config conforms to our expected schema.
     """
-    with resources.open_text('premiscale.config.data', schema) as schema_f:
+    with resources.open_text('src.premiscale.config.data', schema) as schema_f:
         schema = yamale.make_schema(schema_f.name)
 
     data = yamale.make_data(config)
@@ -127,7 +127,7 @@ def _make_default(path: Union[str, Path], default_config: Union[str, Path] = 'de
             Path.mkdir(Path(path).parent, parents=True)
         if not _config_exists(path):
             log.debug(f'Creating default config file at \'{str(path)}\'')
-            with resources.open_text('premiscale.config.data', str(default_config)) as default_f, open(str(path), 'x', encoding='utf-8') as f:
+            with resources.open_text('src.premiscale.config.data', str(default_config)) as default_f, open(str(path), 'x', encoding='utf-8') as f:
                 f.write(default_f.read().strip())
             log.debug(f'Successfully created default config file at \'{str(path)}\'')
     except PermissionError:
