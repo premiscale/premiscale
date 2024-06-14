284d11c872e115b22c35fb18a4b03e0a4c4d14ad
Emma Doyle <emma.ann.doyle@gmail.com>
2024-06-14 15:31:24 -0400

    store-vm-data: parse returned vm metrics

---
+++ src/premiscale/hypervisor/_schemas.py
@@ -0,0 +1,116 @@
+"""
+Provide dataclasses for parsing retrieved hypervisor objects.
+"""
+
+
+from __future__ import annotations
+
+import logging
+
+from attrs import define
+from attr import ib
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+
+
+log = logging.getLogger(__name__)
+
+
+@define
+class vCPU:
+    """
+    A dataclass for storing CPU statistics.
+    """
+    state: int
+    time: int
+    wait: int
+    delay: int
+
+
+@define
+class Net:
+    """
+    A dataclass for storing network statistics.
+    """
+    name: str
+    rx_bytes: int
+    rx_pkts: int
+    rx_errs: int
+    rx_drop: int
+    tx_bytes: int
+    tx_pkts: int
+    tx_errs: int
+    tx_drop: int
+
+
+@define
+class Block:
+    """
+    A dataclass for storing block statistics.
+    """
+    name: str
+    path: str
+    backingIndex: int
+    rd_reqs: int
+    rd_bytes: int
+    rd_times: int
+    wr_reqs: int
+    wr_bytes: int
+    wr_times: int
+    fl_reqs: int
+    fl_times: int
+    allocation: int
+    capacity: int
+    physical: int
+
+
+@define
+class DomainStats:
+    """
+    A dataclass for storing domain statistics.
+    """
+    name: str
+    state_state: int
+    state_reason: int
+    cpu_time: int
+    cpu_user: int
+    cpu_system: int
+    cpu_cache_monitor_count: int
+    cpu_haltpoll_success_time: int
+    cpu_haltpoll_fail_time: int
+    balloon_current: int
+    balloon_maximum: int
+    balloon_swap_in: int
+    balloon_swap_out: int
+    balloon_major_fault: int
+    balloon_minor_fault: int
+    balloon_unused: int
+    balloon_available: int
+    balloon_usable: int
+    balloon_last_update: int
+    balloon_disk_caches: int
+    balloon_hugetlb_pgalloc: int
+    balloon_hugetlb_pgfail: int
+    balloon_rss: int
+    vcpu_current: int
+    vcpu_maximum: int
+    vcpu: List[vCPU]
+    net: List[Net]
+    block: List[Block]
+    dirtyrate_calc_status: int
+    dirtyrate_calc_start_time: int
+    dirtyrate_calc_period: int
+    net_count: int | None = ib(default=None)
+    block_count: int | None = ib(default=None)
+
+    def __attrs_post_init__(self) -> None:
+        if self.block_count is None:
+            self.block_count = len(self.block)
+
+        if self.net_count is None:
+            self.net_count = len(self.net)
+
+        if self.vcpu_current != self.vcpu_maximum:
+            log.warning(f'vCPU count disparity for {self.name}: {self.vcpu_current} current != {self.vcpu_maximum} max. ')
--- src/premiscale/hypervisor/qemu.py
+++ src/premiscale/hypervisor/qemu.py
@@ -10,15 +10,16 @@ import logging
 from typing import TYPE_CHECKING
 from libvirt import (
     libvirtError,
-    VIR_CONNECT_GET_ALL_DOMAINS_STATS_RUNNING,
+    # VIR_CONNECT_GET_ALL_DOMAINS_STATS_RUNNING,
     VIR_DOMAIN_RUNNING
 )
 from premiscale.hypervisor._base import Libvirt
-
+from premiscale.hypervisor._schemas import vCPU, Net, Block, DomainStats
 
 if TYPE_CHECKING:
-    from typing import Dict
+    from typing import Dict, List
     from ipaddress import IPv4Address
+
 
 log = logging.getLogger(__name__)
 
@@ -94,22 +95,90 @@ class Qemu(Libvirt):
         if self._connection is None:
             return {}
 
-        stats: Dict[str, Dict] = {
-            'vmStats': {}
+        stats: Dict[str, List] = {
+            'vmStats': []
         }
 
-        domains = self._connection.listAllDomains()
+        all_domain_stats = self._connection.getAllDomainStats(
+            # https://vscode.dev/github/premiscale/premiscale/blob/store-vm-dataremiscale/lib/python3.10/site-packages/libvirt.py#L6424-L6425
+            # Using 0 for @stats returns all stats groups supported by the given hypervisor.
+            stats=0,
+            flags=VIR_DOMAIN_RUNNING
+        )
 
-        for domain in domains:
-            domain_name = domain.name()
-            vm_conf = self._connection.lookupByName(domain_name)
+        # Normalize domains' stat fields into a list of DomainStats objects.
+        domain_stats_filtered_list: List[DomainStats] = []
 
-            log.debug(f"VM: {vm_conf.name()}")
+        for domain_stats in all_domain_stats:
+            domain, stat = domain_stats
+            stat['name'] = domain.name()
 
-            if domain.isActive():
-                stats['vmStats'][domain_name] = {
-                    'cpu': vm_conf.getCPUStats(True),
-                    'memory': vm_conf.memoryStats()
-                }
+            domain_keys = stat.keys()
+            domain_stats_filtered = {}
+            vcpus: List[Dict[str, int]] = []
+            blocks: List[Dict[str, int]] = []
+            nets: List[Dict[str, int]] = []
+
+            # Normalize all the returned stats fields and parse them into a DomainStats object.
+            for key in domain_keys:
+                # Replace '-' and '.' with '_' in the keys for consistency.
+                oldKey = key
+                key.replace('-', '_')
+                key.replace('.', '_')
+                stat[key] = stat.pop(oldKey)
+
+                # Bin parsed vCPU, Block, and Net stats to form the proper hierarchy.
+                if key.startswith('vcpu_') and key != 'vcpu_current' and key != 'vcpu_maximum':
+                    try:
+                        index = int(key.split('_')[1])
+                    except IndexError as i:
+                        log.error(f'IndexError: {i}')
+                        return {}
+
+                    while True:
+                        if index >= len(vcpus):
+                            vcpus.append({})
+                        else:
+                            break
+
+                    vcpus[index][key] = stat[key]
+                elif key.startswith('block_') and key != 'block_count':
+                    try:
+                        index = int(key.split('_')[1])
+                    except IndexError as i:
+                        log.error(f'IndexError: {i}')
+                        return {}
+
+                    while True:
+                        if index >= len(blocks):
+                            blocks.append({})
+                        else:
+                            break
+
+                    blocks[index][key] = stat[key]
+                elif key.startswith('net_') and key != 'net_count':
+                    try:
+                        index = int(key.split('_')[1])
+                    except IndexError as i:
+                        log.error(f'IndexError: {i}')
+                        return {}
+
+                    while True:
+                        if index >= len(nets):
+                            nets.append({})
+                        else:
+                            break
+
+                    nets[index][key] = stat[key]
+                else:
+                    domain_stats_filtered[key] = stat[key]
+            else:
+                domain_stats_filtered['vcpu'] = vcpus
+                domain_stats_filtered['block'] = blocks
+                domain_stats_filtered['net'] = nets
+
+            domain_stats_filtered_list.append(
+                DomainStats(**domain_stats_filtered)
+            )
 
         return stats
