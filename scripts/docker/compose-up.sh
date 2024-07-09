374c9504e4f7b6ac545395476405c339446c76ee
Emma Doyle <emma.ann.doyle@gmail.com>
2024-07-09 12:49:03 -0400

    AGENT-121: Add grafana config

--- scripts/docker/compose-up.sh
+++ scripts/docker/compose-up.sh
@@ -53,7 +53,7 @@ decrypt_env
 if [ ! -d "certs/" ]; then
     mkdir certs/
     # https://medium.com/@maciej.skorupka/hostname-mismatch-ssl-error-in-python-2901d465683
-    openssl req -x509 -newkey rsa:4096 -keyout certs/key.pem -out certs/cert.pem -sha256 -days 10 -nodes -subj "/C=US/ST=Massachusetts/L=Chelsea/O=PremiScale/CN=platform/emailAddress=$(git config --global --get user.email)" -addext "subjectAltName=DNS:platform"
+    openssl req -x509 -newkey rsa:4096 -keyout certs/key.pem -out certs/cert.pem -sha256 -days 10 -nodes -subj "/C=US/ST=Massachusetts/L=Chelsea/O=PremiScale/CN=platform/emailAddress=$(git config --global --get user.email)" -addext "subjectAltName=DNS:platform,DNS:grafana"
 fi
 
 
